9ec42b9b8d1887b418b2093d730c1cfc83c19a10
Liam Bard <liam.axel.bard@gmail.com>
2024-05-20 23:25:24 +0200

    vitest

--- src/utils/list.js
+++ src/utils/list.js
@@ -4,8 +4,12 @@ function splitTodosIntoDays(todos) {
 	const mo = todos.filter(t => t.day === 'mo')
 	const ti = todos.filter(t => t.day === 'ti')
 	const on = todos.filter(t => t.day === 'on')
+	const to = todos.filter(t => t.day === 'to')
+	const fr = todos.filter(t => t.day === 'fr')
+	const lö = todos.filter(t => t.day === 'lö')
+	const sö = todos.filter(t => t.day === 'sö')
 
-	return [mo, ti, on]
+	return [mo, ti, on, to, fr, lö, sö]
 }
 
 // Tips! Du kan få användning för funktioner som:
